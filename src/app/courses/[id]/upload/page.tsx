b346845477758eb2116f645aa791c89ee24d2862
ftlongsandwich <fr4nklinszhang@gmail.com>
2025-01-18 17:40:53 -0500

    client side file upload

--- src/app/courses/[id]/upload/page.tsx
+++ src/app/courses/[id]/upload/page.tsx
@@ -1,5 +1,6 @@
 import { auth } from "@clerk/nextjs/server";
 import { Button } from "@/components/ui/button";
+import { FileUploader } from "@/components/upload"
 
 export default async function UploadPage({
   params,
@@ -21,7 +22,8 @@ export default async function UploadPage({
           <div className="text-gray-600">
             Drag and drop your files here, or click to select files
           </div>
-          <Button>Select Files</Button>
+          {/* <Button>Select Files</Button> */}
+          <FileUploader/>
         </div>
       </div>
     </div>
